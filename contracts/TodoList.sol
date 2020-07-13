fbd08fb08fe8a48deee042d59c67df9ab47d2a76
Florence Alonzeau <florencealonzeau18@gmail.com>
2020-07-13 15:35:48 +0200

    first blockchain

---
+++ contracts/TodoList.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+/// Create Smart Contract That will manage the TodoList.
+
+ contract TodoList { 
+    uint public taskCount= 0;
+}
