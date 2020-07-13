1685ce4dc4649c1794aaafba027f41d85748d1b0
Florence Alonzeau <florencealonzeau18@gmail.com>
2020-07-13 18:00:40 +0200

    tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -12,13 +12,19 @@ pragma solidity >=0.4.21 <0.7.0;
     bool completed; 
 } 
  mapping(uint => Task) public tasks;
-
+   
+   event TaskCreated(
+       uint id,
+       string content,
+       bool completed
+   );
   constructor() public{
       createTask("Checkout");
   }
   function createTask(string memory _content) public{
       taskCount ++;
       tasks[taskCount] = Task(taskCount, _content, false);
+      emit TaskCreated(taskCount, _content, false);
   }
   
 }
